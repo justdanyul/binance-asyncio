7568f7542760b2bdb539aa55f751d04febbe8b48
Daniel Mouritsen <daniel.mouritsen@gmail.com>
2021-02-21 22:46:08 +0000

    added documenation for MarketDataEndpoints

--- binance_asyncio/endpoints.py
+++ binance_asyncio/endpoints.py
@@ -179,7 +179,7 @@ class MarketDataEndpoints(BaseClient):
             containing all recent trades 
 
             .. code-block::
-            
+
                 [
                     {
                         "id": 82457,
@@ -202,6 +202,38 @@ class MarketDataEndpoints(BaseClient):
 
 
     async def get_aggregated_trades(self, symbol: str, from_id=None, start_time=None, end_time=None, limit=500):
+        """
+        Get aggregate trades.
+
+        :param symbol: The symbol of the pair
+        :param limit: The maximum number results wanted. It default to 500 , the maximum is 1000.
+        :param from_id: This is the tradeid to get aggregate trades from (inclusive), if none is provided, it just gets most recent trades
+        :param start_time: The time to begin the aggregation from. For example, '10 minutes ago' or '1 second ago'
+        :param end_time: The time to end the aggregation, in similar format as above
+        :type symbol: string
+        :type limit: int
+        :type start_time: string
+        :type end_time: string
+        :rtype: (int, list) 
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element is a list 
+            containing the aggregated trades
+
+            .. code-block::
+            
+                [
+                    {
+                        "a": 16239,         // agg tradeId
+                        "p": "0.01633102",  // price
+                        "q": "6.73443515",  // quantity
+                        "f": 13781,         // the first tradeId
+                        "l": 13781,         // the last tradeId
+                        "T": 1498794537053, // timestamp
+                        "m": true,          // is the buyer the maker?
+                        "M": true           // was the trade the best price match?
+                    }
+                ]
+        """
         return await self._get('aggTrades', 
             RequestBuilder()
                 .with_symbol(symbol)
@@ -213,6 +245,60 @@ class MarketDataEndpoints(BaseClient):
                 .get_params())
 
     async def get_klines(self, symbol: str, interval='1m', start_time=None, end_time=None, limit=500):
+        """
+        Get kline/candlestick bars for a symbol
+
+        :param symbol: The symbol of the pair
+        :param interval: The interval of the kline, valid intervals are
+
+            - "1m"
+            - "3m"
+            - "5m"
+            - "15m"
+            - "30m"
+            - "1h"
+            - "2h"
+            - "4h"
+            - "6h"
+            - "8h"
+            - "12h"
+            - "1d"
+            - "3d"
+            - "1w"
+            - "1M"           
+        
+        :param limit: The maximum number of klines wanted. It default to 500 , the maximum is 1000.
+        :param start_time: The time to get klines from from. For example, '10 minutes ago' or '1 second ago'
+        :param end_time: The time to get klines until, in similar format as above
+        :type symbol: string
+        :type limit: int
+        :type interval: string
+        :type start_time: string
+        :type end_time: string
+        :rtype: (int, list) 
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element is a list 
+            containing the klines
+
+            .. code-block::
+            
+                [
+                    [
+                        1499040000000,      // Open time
+                        "0.01634790",       // Open
+                        "0.80000000",       // High
+                        "0.01575800",       // Low
+                        "0.01577100",       // Close
+                        "148976.11427815",  // Volume
+                        1499644799999,      // Close time
+                        "2434.19055334",    // Quote asset volume
+                        308,                // Number of trades
+                        "1756.87402397",    // Taker buy base asset volume
+                        "28.46694368",      // Taker buy quote asset volume
+                        "17928899.62484339" // Ignore.
+                    ]
+                ]
+        """        
         return await self._get('klines', RequestBuilder()
                 .with_symbol(symbol)
                 .with_limit(limit)
@@ -223,18 +309,98 @@ class MarketDataEndpoints(BaseClient):
                 .get_params())
 
     async def get_current_average(self, symbol: str):
+        """
+        Get the current average price for a symbol.
+
+        :param symbol: The symbol of the pair
+        :type symbol: string
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element dictionary representing the response
+
+            .. code-block::
+            
+                {
+                    "mins": 5,
+                    "price": "9.35751834"
+                }
+        """        
         return await self._get('avgPrice',
             RequestBuilder().with_symbol(symbol).build().get_params())
 
     async def get_price_change_stats_ticker(self, symbol: str):
+        """
+        Get the 24 hour rolling window price change statistics.
+
+        :param symbol: The symbol of the pair
+        :type symbol: string
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element dictionary representing the response
+
+            .. code-block::
+            
+                {
+                    "symbol": "BNBBTC",
+                    "priceChange": "-94.99999800",
+                    "priceChangePercent": "-95.960",
+                    "weightedAvgPrice": "0.29628482",
+                    "prevClosePrice": "0.10002000",
+                    "lastPrice": "4.00000200",
+                    "lastQty": "200.00000000",
+                    "bidPrice": "4.00000000",
+                    "askPrice": "4.00000200",
+                    "openPrice": "99.00000000",
+                    "highPrice": "100.00000000",
+                    "lowPrice": "0.10000000",
+                    "volume": "8913.30000000",
+                    "quoteVolume": "15.30000000",
+                    "openTime": 1499783499040,
+                    "closeTime": 1499869899040,
+                    "firstId": 28385,   // First tradeId
+                    "lastId": 28460,    // Last tradeId
+                    "count": 76         // Trade count
+                }
+        """          
         return await self._get('ticker/24hr', 
             RequestBuilder().with_symbol(symbol).build().get_params())
 
     async def get_symbol_price_ticker(self, symbol: str):
+        """
+        Get the latest price for a symbol or symbols
+
+        :param symbol: The symbol of the pair
+        :type symbol: string
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element dictionary representing the response
+
+            .. code-block::
+            
+                {
+                    "symbol": "LTCBTC",
+                    "price": "4.00000200"
+                }
+        """             
         return await self._get('ticker/price', 
             RequestBuilder().with_symbol(symbol).build().get_params())
 
     async def get_symbol_order_book_ticker(self, symbol: str):
+        """
+        Get the best price/qty on the order book for a symbol or symbols
+
+        :param symbol: The symbol of the pair
+        :type symbol: string
+        :return: returns a tuple, where the first element is the HTTP 
+            response status code and the second element dictionary representing the response
+
+            .. code-block::
+            
+                {
+                    "symbol": "LTCBTC",
+                    "bidPrice": "4.00000000",
+                    "bidQty": "431.00000000",
+                    "askPrice": "4.00000200",
+                    "askQty": "9.00000000"
+                }
+        """           
         return await self._get('ticker/bookTicker', 
             RequestBuilder().with_symbol(symbol).build().get_params())
 
